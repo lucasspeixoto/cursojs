f053c7136f5e153fd8ee42cdd1fdc295f59d2652
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-01-25 17:38:57 -0300

    object

---
+++ fundamentos/objeto.js
@@ -0,0 +1,19 @@
+const pessoa1 = {}
+
+pessoa1.nome = 'Liana'
+pessoa1.idade = 27
+pessoa1['Altura'] = 1.65 // Outra forma de atríbuir uma parâmetro para um objeto (Evitar Atributos com espaço)
+
+console.log('Pessoa 1: ')
+console.log(pessoa1)
+
+const pessoa2 = {
+    nome: 'Lucas',
+    idade: 29,
+    Altura: 1.72 
+}
+
+
+console.log('Pessoa 2: ')
+console.log(pessoa2)
+
