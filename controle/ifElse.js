4e4e6b5a615ac7a4ae1eb4b65a29a8b8a27053ac
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-02-03 22:45:54 -0300

    aula if/else

---
+++ controle/ifElse.js
@@ -0,0 +1,31 @@
+function tratarErroELancar(erro) {
+
+    throw console.log('Digite um númer')
+}
+
+
+
+const imprimirResultado = function (nota) {
+    //  Is Not a Number
+    if (!isNaN(nota) == false) {
+        console.log('Digitar Nota númerica')
+        return
+
+    }
+    if (nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado('Epa') //por JS ser fracamente tipada, como 'Epa' não é > 7, cai no bloco else
+
+var valor = 1.5;
+var teste = "a";
+
+console.log(!isNaN(valor)); /* retorna false negando fica true, 1.5 é tipo numérico */
+console.log(!isNaN(teste)); /* retorna true negando fica falso, "a" não é um número */
+
+console.log(!isNaN('10'))
