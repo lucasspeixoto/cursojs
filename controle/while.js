dae4975e1d8faffe51a8e9352eed3161168785a1
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-02-04 08:22:44 -0300

    aula while

---
+++ controle/while.js
@@ -0,0 +1,14 @@
+function getInt(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1){
+    opcao = getInt(-1,10)
+    console.log(`Opção escolhida foi: ${opcao}`)
+}
+
+console.log('fim')
+
