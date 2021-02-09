272c3779da4b841d7f10de395cdd6ee39c919a53
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-02-08 22:00:15 -0300

    aula this pode variar

---
+++ funcao/thisPodeVariar.js
@@ -0,0 +1,19 @@
+/* this, assim como self em python, é uma palavra reservada
+que serve para referenciar um objeto atual de uma execução
+
+Em JS o this pode variar conforme a forma que a função é chamada.
+O this é sempre o objeto que está sendo referenciado naquela contexto de execução
+
+*/
+
+/* Objetivos para a criação de arrow functions
+    * Simplificação da chamada (Diminuir a sintaxe)
+    * this não variar (Ser constante) conforme a chamada (contexto léxico - Aquele no qual a função foi chamada)
+
+Em funções declaradas da forma tradicional function f3() {}, 
+o this pode variar conforme quem chamou a função, a menos que 
+usemos uma função arrow 
+
+*/
+
+
