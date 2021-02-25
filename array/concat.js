4641734949a54ed3e32c04fe427793a4d80345c3
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-02-24 22:50:23 -0300

    Aula concat

---
+++ array/concat.js
@@ -0,0 +1,8 @@
+const filhos = ['Cesar', 'Rubens']
+const filhas = ['Cecília', 'Alice']
+
+const todos = filhas.concat(filhos, 'Fulano')
+
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
